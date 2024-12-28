07def783fdd21df44df59d615f869a6241ee99ba
Touko Peltomaa <touko.peltomaa@gmail.com>
2024-12-28 20:10:24 +0200

    feat: allow nav between panes with ctrl only

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -6,6 +6,13 @@ vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
+-- Display line number
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.signcolumn = "number"
+
+-- Allow navigating between panes using only ctrl
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to the left pane" })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to the pane below" })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to the pane above" })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to the right pane" })
