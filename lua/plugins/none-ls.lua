10a783e3ca52515aed27f81c85c2bfb956a741e0
Touko Peltomaa <touko.peltomaa@gmail.com>
2025-02-15 15:47:09 +0200

    fix: Improve web lsp config

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -6,7 +6,6 @@ return {
       sources = {
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.prettier,
-        null_ls.builtins.formatting.eslint_d,
         null_ls.builtins.formatting.gofumpt,
         null_ls.builtins.formatting.goimports,
         null_ls.builtins.diagnostics.golangci_lint,
