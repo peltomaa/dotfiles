0c3aa40fd0d5f450ed34ce3d6bc7469e503ad076
Touko Peltomaa <touko.peltomaa@gmail.com>
2025-02-15 15:40:18 +0200

    feat: add git blame functionality

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,17 +1,42 @@
 return {
-	{
-		"kdheepak/lazygit.nvim",
-		cmd = "LazyGit",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-		},
-		keys = {
-			{ "<leader>gg", "<cmd>LazyGit<cr>", desc = "Open LazyGit" },
-		},
-		config = function()
-			vim.g.lazygit_floating_window_winblend = 0
-			vim.g.lazygit_floating_window_scaling_factor = 0.8
-			vim.g.lazygit_use_neovim_remote = true
-		end,
-	},
+  {
+    "kdheepak/lazygit.nvim",
+    cmd = "LazyGit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    keys = {
+      { "<leader>gg", "<cmd>LazyGit<cr>", desc = "Open LazyGit" },
+    },
+    config = function()
+      vim.g.lazygit_floating_window_winblend = 0
+      vim.g.lazygit_floating_window_scaling_factor = 0.8
+      vim.g.lazygit_use_neovim_remote = true
+    end,
+  },
+
+  {
+    "lewis6991/gitsigns.nvim",
+    keys = {
+      { "<leader>ghb", "<cmd>Gitsigns blame_line<cr>", desc = "Blame Line" },
+      { "<leader>ghB", "<cmd>Gitsigns blame<cr>",      desc = "Blame Buffer" },
+    },
+    opts = {
+      signs = {
+        add = { text = "▎" },
+        change = { text = "▎" },
+        delete = { text = "" },
+        topdelete = { text = "" },
+        changedelete = { text = "▎" },
+        untracked = { text = "▎" },
+      },
+      signs_staged = {
+        add = { text = "▎" },
+        change = { text = "▎" },
+        delete = { text = "" },
+        topdelete = { text = "" },
+        changedelete = { text = "▎" },
+      },
+    },
+  },
 }
