244972e709b6716bfe9595e5791d0796a7236052
peltomaa <touko.peltomaa@gmail.com>
2025-04-24 19:48:05 +0300

    feat: Add rust

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,32 +1,34 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
-    local configs = require("nvim-treesitter.configs")
-    configs.setup({
-      ensure_installed = {
-        "c",
-        "lua",
-        "vim",
-        "vimdoc",
-        "javascript",
-        "typescript",
-        "tsx",
-        "html",
-        "css",
-        "go",
-        "gomod",
-        "gowork",
-        "gosum",
-        "yaml",
-        "markdown",
-        "python",
-        "php",
-        "haskell",
-      },
-      sync_install = false,
-      highlight = { enable = true },
-      indent = { enable = true },
-    })
-  end,
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local configs = require("nvim-treesitter.configs")
+		configs.setup({
+			ensure_installed = {
+				"c",
+				"lua",
+				"vim",
+				"vimdoc",
+				"javascript",
+				"typescript",
+				"tsx",
+				"html",
+				"css",
+				"go",
+				"gomod",
+				"gowork",
+				"gosum",
+				"yaml",
+				"markdown",
+				"python",
+				"php",
+				"haskell",
+				"rust",
+				"ron",
+			},
+			sync_install = false,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
 }
